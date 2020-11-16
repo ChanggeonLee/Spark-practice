656cf9ba0a29e2bba36ea587aef9c578077b4176
changgeonLee <changgeon718@gmail.com>
2020-11-17 01:18:42 +0900

    bug fix

--- worstMoviesSpark.py
+++ worstMoviesSpark.py
@@ -17,7 +17,7 @@ def parseInput(line):
 
 if __name__ == "__main__":
   movies = loadMovies()
-  path = "hdfs://user/marai_dev/ml-lastest-small/ratings.csv"
+  path = "hdfs:///user/maria_dev/ml-latest-small/ratings.csv"
 
   # create spark context
   conf = SparkConf().setAppName("WorstMovies")
@@ -34,7 +34,6 @@ if __name__ == "__main__":
   # line --> (movieId, (rating, 1.0))
   ratings = lines.map(parseInput)
 
-
   # reduc to (movieId, (sumOfRating, countRating))
   sumAndCounts = ratings.reduceByKey(lambda m1, m2:(m1[0]+m2[0], m1[1]+m2[1]))
 
@@ -42,10 +41,10 @@ if __name__ == "__main__":
   avrageRatings = sumAndCounts.mapValues(lambda v: v[0]/v[1])
 
   # sort
-  sortedMovies = avrageRatings.srotBy(lambda x : x[1])
+  sortedMovies = avrageRatings.sortBy(lambda x : x[1])
 
   # top 10
-  result = sortedMovies.take(10)
+  results = sortedMovies.take(10)
 
-  #for result in results:
-  print(movies[result[0]], result[1])
+  for result in results:
+    print(movies[result[0]], result[1])
